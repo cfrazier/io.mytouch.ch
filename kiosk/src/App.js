2d6d4f23006987bf6bd63fe27ff1933274cf16cc
Christopher Frazier <chris.frazier@fraziermedia.com>
2020-06-19 16:01:43 -0700

    Kiosk more or less works.

--- kiosk/src/App.js
+++ kiosk/src/App.js
@@ -7,29 +7,82 @@ import "./styles/App.scss";
 
 const App = () => {
 	return (
-		<Router className="App">
-			<Switch>
-				<Route path="/kiosk/:organizationId">
-					<Kiosk />
-				</Route>
-				<Route path="/">Please log in to the admin to get started...</Route>
-			</Switch>
-		</Router>
+		<div className="App">
+			<Router>
+				<Switch>
+					<Route path="/kiosk/:organizationId">
+						<Kiosk />
+					</Route>
+					<Route path="/">Please log in to the admin to get started...</Route>
+				</Switch>
+			</Router>
+		</div>
 	);
 };
 
 export default App;
 
-const Kiosk = (props) => {
+export const Kiosk = (props) => {
 	const { organizationId } = useParams();
-	const [organization, setOrganization] = useState();
+	const [venues, setVenues] = useState();
 
-	useEffect(() => {});
+	const clientURL = new URL(process.env.PUBLIC_URL, window.location.href);
+	const { hostname } = clientURL;
+	const serverURL = `https://${hostname}`;
 
 	const updateOrganization = () => {
 		if (organizationId) {
-			httpFetch("get", `/api/organization/${organizationId}/venues`)
+			httpFetch(
+				"get",
+				`/api/organizations/${organizationId}/venues`,
+				null,
+				(error, response) => {
+					if (error) return console.log(error);
+					if (response.error) return console.log(response.error);
+					setVenues(response);
+				}
+			);
 		}
-	}
-	return <div className="Kiosk">{organizationId}</div>;
+	};
+
+	useInterval(updateOrganization, 5000);
+
+	return (
+		<div className="Kiosk">
+			{venues ? (
+				<div className="Active">
+					<table>
+						<thead>
+							<tr>
+								<th className="Name">Check in at {serverURL}/checkin</th>
+								<th className="Code">Code</th>
+								<th className="Available">Available</th>
+							</tr>
+						</thead>
+						<tbody>
+							{venues.map((venue) => (
+								<tr key={`venue_${venue._id}`} className="Venue">
+									<td>
+										<div className="Name">{venue.name}</div>
+										<div className="Description">{venue.description}</div>
+									</td>
+									<td>{venue.code.toUpperCase()}</td>
+									<td>
+										<div
+											className="Badge"
+											style={{ backgroundColor: venue.color }}
+										>
+											{venue.available}
+										</div>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			) : (
+				<div className="Loading">Loading</div>
+			)}
+		</div>
+	);
 };
