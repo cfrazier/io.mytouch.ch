94508cd13dfb9ebd92bb29e0d5a114ae85a44ac9
Christopher Frazier <chris.frazier@fraziermedia.com>
2020-06-19 08:29:13 -0700

    Kiosk bootstrap

--- kiosk/src/App.js
+++ kiosk/src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import './styles/App.scss';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route, useParams } from "react-router-dom";
+import useInterval from "use-interval";
+import httpFetch from "./services/http";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+import "./styles/App.scss";
+
+const App = () => {
+	return (
+		<Router className="App">
+			<Switch>
+				<Route path="/kiosk/:organizationId">
+					<Kiosk />
+				</Route>
+				<Route path="/">Please log in to the admin to get started...</Route>
+			</Switch>
+		</Router>
+	);
+};
 
 export default App;
+
+const Kiosk = (props) => {
+	const { organizationId } = useParams();
+	const [organization, setOrganization] = useState();
+
+	useEffect(() => {});
+
+	const updateOrganization = () => {
+		if (organizationId) {
+			httpFetch("get", `/api/organization/${organizationId}/venues`)
+		}
+	}
+	return <div className="Kiosk">{organizationId}</div>;
+};
